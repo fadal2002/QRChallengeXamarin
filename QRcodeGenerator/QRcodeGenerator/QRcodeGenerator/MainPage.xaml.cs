c5a0e2d0ce6aa9df898f3b9248c86e4cefdb76b7
fadal2002 <fadalalenazi1@gmail.com>
2020-05-17 17:33:02 +0100

    Final submission

---
+++ QRcodeGenerator/QRcodeGenerator/QRcodeGenerator/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QRcodeGenerator
+{
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void generate_Button_Clicked(object sender, EventArgs e)
+        {
+            //Handling the generate button click event
+            String textVal = QRTextEntry.Text;
+            //Checking if the user has inputted some text to convert or not before generating 
+            if(textVal == null || textVal == "")
+            {
+                //Display an alert if the user tries to generate without entering a text to convert
+                await DisplayAlert("Alert", "You need to enter some text!", "OK");
+            }
+            else
+            {
+                //Generate a QR code and reset the entry text 
+                QRImage.BarcodeValue = textVal;
+                QRTextEntry.Text = "";
+            }
+        }
+
+        async private void scan_Button_Clicked(object sender, EventArgs e)
+        {
+            //Handling the scan button click event
+            //Open the scan page when scan button is clicked
+            await Navigation.PushAsync(new ScanPage());
+        }
+    }
+}
---
+++ QRcodeGenerator/QRcodeGenerator/QRcodeGenerator/ScanPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QRcodeGenerator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScanPage : ContentPage
+    {
+        public ScanPage()
+        {
+            InitializeComponent();
+        }
+
+        private void scanView_OnScanResult(ZXing.Result result)
+        {
+            //Handling the QR code scanner's on scan result event 
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                //Set the result label text to the result returned from the QR code scanner
+                resultLabel.Text = "Result: " + result.Text;
+            });
+
+        }
+    }
+}
